a0f7b44ee2fd3db6075ad8ea97042fcd0ef226e4
schiff23 <cerfblanc73@gmail.com>
2021-04-22 14:54:16 +0800

    修改update.py

---
+++ docs/语言/大学英语IV.md
@@ -0,0 +1,6 @@
+
+
+## 文件列表
+
+- 大学英语IV
+    - [B4 综合教程教师用书.pdf](https://github.com/ballsnow/zju-open-course/raw/master/./语言/大学英语IV/B4 综合教程教师用书.pdf)
--- update.py
+++ update.py
@@ -2,46 +2,48 @@ import os
 
 EXCLUDE_DIRS = ['.git', 'docs', '.vscode', '.circleci']
 README_MD = ['README.md', 'readme.md', 'index.md']
+COURSE_CATA = ['数学', '科学', '计算机', '语言', '技能', '通识']
 
 TXT_EXTS = ['md', 'txt']
-TXT_URL_PREFIX = 'https://github.com/QSCTech/zju-icicles/blob/master/'
-BIN_URL_PREFIX = 'https://github.com/QSCTech/zju-icicles/raw/master/'
+TXT_URL_PREFIX = 'https://github.com/ballsnow/zju-open-course/blob/master/'
+BIN_URL_PREFIX = 'https://github.com/ballsnow/zju-open-course/raw/master/'
 
 
 def list_files(course: str):
-    filelist_texts = '## 文件列表\n\n'
-    readme_path = ''
-    for root, dirs, files in os.walk(course):
-        level = root.replace(course, '').count(os.sep)
-        indent = ' ' * 4 * level
-        filelist_texts += '{}- {}\n'.format(indent, os.path.basename(root))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            if f not in README_MD:
-                if f.split('.')[-1] in TXT_EXTS:
-                    filelist_texts += '{}- [{}]({})\n'.format(subindent,
-                                                              f, '{}{}/{}'.format(TXT_URL_PREFIX, root,  f))
-                else:
-                    filelist_texts += '{}- [{}]({})\n'.format(subindent,
-                                                              f, '{}{}/{}'.format(BIN_URL_PREFIX, root,  f))
-            else:
-                readme_path = '{}/{}'.format(root, f)
-    return filelist_texts, readme_path
+	filelist_texts = '## 文件列表\n\n'
+	readme_path = ''
+	for root, dirs, files in os.walk(course):
+		level = root.replace(course, '').count(os.sep)
+		indent = ' ' * 4 * level
+		filelist_texts += '{}- {}\n'.format(indent, os.path.basename(root))
+		subindent = ' ' * 4 * (level + 1)
+		for f in files:
+			if f not in README_MD:
+				if f.split('.')[-1] in TXT_EXTS:
+					filelist_texts += '{}- [{}]({})\n'.format(subindent,
+															  f, '{}{}/{}'.format(TXT_URL_PREFIX, root,  f))
+				else:
+					filelist_texts += '{}- [{}]({})\n'.format(subindent,
+															  f, '{}{}/{}'.format(BIN_URL_PREFIX, root,  f))
+			else:
+				readme_path = '{}/{}'.format(root, f)
+	return filelist_texts, readme_path
 
 
-def generate_md(course: str, filelist_texts: str, readme_path: str):
-    final_texts = ['\n\n', filelist_texts]
-    if readme_path:
-        with open(readme_path, 'r') as file:
-            final_texts = file.readlines() + final_texts
-    with open('docs/{}.md'.format(course), 'w') as file:
-        file.writelines(final_texts)
+def generate_md(course_path: str, filelist_texts: str, readme_path: str):
+	final_texts = ['\n\n', filelist_texts]
+	if readme_path:
+		with open(readme_path, 'r', encoding="utf-8") as file:			
+			final_texts = file.readlines() + final_texts
+	with open('./docs/{}.md'.format(course_path), 'w+') as file:
+		file.writelines(final_texts)
 
 
 if __name__ == '__main__':
-    courses = list(filter(lambda x: os.path.isdir(x) and (
-        x not in EXCLUDE_DIRS), os.listdir('.')))  # list courses
-
-    for course in courses:
-        filelist_texts, readme_path = list_files(course)
-        generate_md(course, filelist_texts, readme_path)
+	# scan certain course type directory 
+	for course_type in COURSE_CATA:
+		courses = list(filter(lambda x: os.path.isdir('./{}/{}'.format(course_type,x)), os.listdir('./'+ course_type+'/')))  # list courses
+		for course in courses:
+			course_path = './{}/{}'.format(course_type,course)
+			filelist_texts, readme_path = list_files(course_path)
+			generate_md(course_path, filelist_texts, readme_path)
